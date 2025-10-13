a6e5cdaad8100f7d5d421453d506ffd490aea226
Pat-Asiste <barrantespatrick@gmail.com>
2025-10-13 17:27:50 -0500

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-var c1-4 = getElementById.value c1-4
-var Und  = getElementById.value Und.
+function resistencia(){
+let c1 = document.getElementById("color1").value;
+let c2 = document.getElementById("color2").value;
+let c3 = document.getElementById("color3").value;
+let multiplicador = document.getElementById("unidad").value;
+let tolerancia = document.getElementById("color4").value;
 
-var R = c1 + c2 + c3
-var resultado = R + Und + c4
-getElementById.innerElement = resultado 
+let R = (c1 + c2) * c3 / multiplicador;
+let resultado = R + " " + tolerancia;
+getElementById.innerElement = resultado;
+};
