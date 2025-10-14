41779750aa80c77496c05f840c4c8fc504141c6e
Pat-Asiste <barrantespatrick@gmail.com>
2025-10-14 07:29:40 -0500

    Actualizar script.js. NEW FORMULA

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function resistencia(){
-let c1 = document.getElementById("color1").value;
-let c2 = document.getElementById("color2").value;
-let c3 = document.getElementById("color3").value;
-let multiplicador = document.getElementById("unidad").value;
+let c1 = parseInt(document.getElementById("color1").value);
+let c2 = parseInt(document.getElementById("color2").value);
+let c3 = parseInt(document.getElementById("color3").value);
+let multiplicador = parseFloat(document.getElementById("unidad").value);
 let tolerancia = document.getElementById("color4").value;
 
 let r = (c1 * 10 + c2) * c3 / multiplicador;
